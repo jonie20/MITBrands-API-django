d057f7d1b22e0ce6baff3d6c49a92d30549992ea
jonie20 <84018132+jonie20@users.noreply.github.com>
2024-01-01 16:22:51 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=10)
     description = models.CharField(max_length=600)
-    price = models.PositiveIntegerField()
-    Image = models.ImageField(upload_to = "ClothProducts")
+    price = models.PositiveIntegerField(max_length=10)
+    Image = models.ImageField(blank=False, null= False, upload_to = "uploads/ClothProducts")
 
     def __str__(self):
         return self.name
